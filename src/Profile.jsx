acd77458b86ba45cb94b676b53917fe3c291ede8
Xavier Rodriguez <97705290+XaviRB@users.noreply.github.com>
2023-10-26 16:11:41 -0700

    profile page is in the works but still running into issues with styling

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,64 +1,62 @@
-
-import  React  from 'react';
+import React, { useState } from 'react';
 import {Navbar} from './components/Navbar';
 import {Footer} from './components/Footer';
-import Sidebar from './components/sidebar/Sidebar'
+import Sidebar from './components/sidebar/Sidebar';
+import userImage from './img/users/Default_User.png';
 
 function Profile() {
+    const [selectedInterests, setSelectedInterests] = useState([]);
+    const interests = ["Books", "Video Games", "Movies", "Sports", "Exercise", "Programming"];
+
+    const handleInterestChange = (event) => {
+        const { value } = event.target;
+        setSelectedInterests(prevInterests => 
+            prevInterests.includes(value) 
+            ? prevInterests.filter(interest => interest !== value) 
+            : [...prevInterests, value]
+        );
+    }
+
     return (
-      <>
-          <Navbar/>
-          <div>
-          {/* Header Section */}
-          {/* Header Javascript */}
-          {/* Main Section */}
-          <section className="main">
-              <Sidebar/>
-              <div className="main-content">
-                  <h2>Profile</h2>
-                  <h4>Customize Your Profile Page</h4>
-                  
-                  <div className="container">
-                      <div className="profile-box">
-                          <div className="profile-text">
-                          </div>
-                          <div className="profile-form">
-                            <div className="column">
-                                <div className="profile-item">
-                                    <label htmlFor="name">Name:</label>
-                                    <input type="text" id="name" name="name" className="input" />
+        <>
+            <Navbar/>
+            <section className="main">
+                <Sidebar/>
+                <div className="profile-main-content">
+                    <h2>Profile</h2>
+                    <div className="profile-user-image" style={{ backgroundImage: `url(${userImage})` }}></div>
+                    <h4>Customize Your Profile Page</h4>
+                    <div className="container">
+                        <div className="profile-content">
+                            <div className="profile-form-box"> {/* First Box for Inputs */}
+                                <div className="profile-form">
+                                    <div className="profile-item">
+                                        <label htmlFor="name">Name:</label>
+                                        <input type="text" id="name" name="name" className="input" />
+                                    </div>
+                                    <div className="profile-item">
+                                        <label htmlFor="pronouns">Pronouns:</label>
+                                        <input type="text" id="pronouns" name="pronouns" className="input" />
+                                    </div>
+                                    <div className="profile-item">
+                                        <label htmlFor="menteeMentor">Mentee/Mentor:</label>
+                                        <input type="text" id="menteeMentor" name="menteeMentor" className="input" />
+                                    </div>
                                 </div>
+                            </div> 
+                            <div className="profile-interests-box"> {/* Second Box for Interests */}
                                 <div className="profile-item">
-                                    <label htmlFor="pronouns">Pronouns:</label>
-                                    <input type="text" id="pronouns" name="pronouns" className="input" />
-                                </div>
-                                <div className="profile-item">
-                                    <label htmlFor="menteeMentor">Mentee/Mentor:</label>
-                                    <input type="text" id="menteeMentor" name="menteeMentor" className="input" />
+                                    <label>Selected Interests:</label>
+                                    <input type="text" value={selectedInterests.join(', ')} readOnly />
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="main-item">
-                   <div className="profile">
-                        <div className="profile-box">
-                            <div className="profile-info">
-                                <h2>Bio/Introduction:</h2>
-                            </div>
-                            <div className="profile-info">
-                               <h2>Interests/Hobbies:</h2>
-                            </div>
-                        </div>
-                      </div>
-                  </div>
-              </div>
-          </section>
-          </div>
-          {/* Footer Section */}
-          <Footer />
-      </>
+            </section>
+            <Footer />
+        </>
     )
-  }
+}
 
-export default Profile   
+export default Profile;
